362ffbd97d7ae515d8042ca69b9a58289dbd751f
akesser <andre@kesser.de>
2023-02-02 13:17:03 +0100

    fix: fix pipeline add test (#16)
    * fix: fix pipeline add test
    Signed-off-by: André Kesser <andre.kesser@dkb.de>

--- test/setup.sh
+++ test/setup.sh
@@ -7,6 +7,9 @@ ${KUBECTL} wait configuration.pkg x-generation --for=condition=Healthy --timeout
 ${KUBECTL} wait configuration.pkg x-generation --for=condition=Installed --timeout 5m
 ${KUBECTL} wait "provider.pkg.crossplane.io/crossplane-contrib-provider-aws" --for=condition=healthy --timeout=300s
 ${KUBECTL} wait --for=condition=established --timeout=300s crd/providerconfigs.aws.crossplane.io
+sleep 5
+${KUBECTL} wait "provider.pkg.crossplane.io/crossplane-contrib-provider-zpa" --for=condition=healthy --timeout=300s
+${KUBECTL} wait --for=condition=established --timeout=300s crd/providerconfigs.zpa.crossplane.io
 
 echo "Creating a secret default provider config"
 cat <<EOF | ${KUBECTL} apply -f -
@@ -34,13 +37,40 @@ spec:
       name: aws-creds
       key: key
 EOF
+echo "Creating a secret default provider zpa config"
+cat <<EOF | ${KUBECTL} apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: zpa-creds
+  namespace: upbound-system
+type: Opaque
+stringData:
+  key: nocreds
+EOF
 
-${KUBECTL} create ns assert-db
-${KUBECTL} create ns assert-bucket
-${KUBECTL} create ns assert-bucket-policy
-${KUBECTL} create ns assert-kms
-${KUBECTL} create ns assert-kms-alias
-${KUBECTL} create ns assert-instance-profile
-${KUBECTL} create ns assert-iam-policy
-${KUBECTL} create ns assert-role-policy-attachment
-${KUBECTL} create ns assert-role
+echo "Creating a default provider zpa config"
+cat <<EOF | ${KUBECTL} apply -f -
+apiVersion: zpa.crossplane.io/v1alpha1
+kind: ProviderConfig
+metadata:
+  name: 123456789101-example
+spec:
+  clientID:
+    source: None
+  clientSecret:
+    source: None
+  customerId: testID
+  host: zpa.local
+EOF
+
+${KUBECTL} create ns assert-db || true
+${KUBECTL} create ns assert-bucket || true
+${KUBECTL} create ns assert-bucket-policy || true
+${KUBECTL} create ns assert-kms || true
+${KUBECTL} create ns assert-kms-alias || true
+${KUBECTL} create ns assert-instance-profile || true
+${KUBECTL} create ns assert-iam-policy || true
+${KUBECTL} create ns assert-role-policy-attachment || true
+${KUBECTL} create ns assert-role || true
+${KUBECTL} create ns assert-zpa || true
